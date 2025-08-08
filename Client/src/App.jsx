aeb285239a40dac7f1c8c0847c786980a8aca9c4
Abhishek Sardar <143304955+Abhishek-sard@users.noreply.github.com>
2025-08-08 07:31:42 +0545

    push

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,73 +1,12 @@
+
 import React from 'react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import ImageSlider from './Components/ImageSlider'
-import Footer from './Components/Footer'
-import Navbar from './Components/Navbar'
-import FeaturedProducts from './Components/FeaturedProduct'
 
-
-import Shop from './Pages/Shop/Shop';
-
-import Blog from './Pages/Blog/Blog';
-
-import Contact from './Pages/Contact/Contact';
-
-import Dashboard from './Dashboard/Dashboard'
-import ProductUploadDashboard from './Components/ProductUploadDashboard'
-
-// Home component with wrapped children
-function Home() {
+const App = () => {
   return (
-    <>
-      <ImageSlider />
-      <FeaturedProducts />
-    </>
-  )
-}
-
-function Shops() {
-  return (
-    <>
-      <Shop />
-    </>
-  )
-}
-
-function Contacts() {
-  return (
-    <>
-      <Contact />
-    </>
-  )
-}
-
-function Blogging() {
-  return (
-    <>
-      <Blog />
-    </>
-  )
-}
-
-
-
-
-// Products page component (no need for separate wrapper)
-function App() {
-  return (
-    <Router>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/Shop" element={<Shop />} />
-        <Route path="/Blog" element={<Blog />} />
-        <Route path="/Contact" element={<Contact />} />
-        <Route path="/dashboard" element={<Dashboard />} /> {/* Add Dashboard route */}
-        <Route path="/DashboardUpload" element={<ProductUploadDashboard/>}/>
-
-      </Routes>
-      <Footer />
-    </Router>
+    <div className='text-center '>
+      <h1 className='text-primary'>Welcome to Radha Rani Puja</h1>
+      
+    </div>
   )
 }
 
